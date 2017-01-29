bdd1878f9d936a86d7a2059576409742d1127d83
Terry Nguyen <terreh@terrehbyte.com>
2017-01-29 00:01:56 -0800

    WIP: rebase me

---
+++ DEVNOTES.md
@@ -0,0 +1,27 @@
+# DEV NOTES
+
+These notes are intended for me to keep track of development.
+
+Think of it as a "ROADMAP" of sorts.
+
+- Guity
+  - Interface Considerations
+    - Working Directory + Index
+      - Staged List
+      - Unstaged / Untracked List
+      - Toolbar
+        - Refresh Button
+        - "Add.." Button
+            - option to include ignored files
+    - Settings Foldout
+      - GitConfig
+        - User
+        - E-Mail
+      - Guity Settings
+        - Status Refresh Interval
+          - on open
+          - every _x_ seconds after opening
+        - Repository Location
+  - Technical Debt
+    - Remove libgit2sharp legacy dependency
+    - 
--- UnityProject/Assets/Guity/Dependencies/Editor/GitWindow.cs
+++ UnityProject/Assets/Guity/Dependencies/Editor/GitWindow.cs
@@ -24,49 +24,42 @@ public class GitRepository
         public string filePath;         // Absolute file path.
     }
 
-    private Repository gitRepo;
+    private string gitRepoPath = @"C:\Users\terryn\Documents\Visual Studio 2015\Projects\UnityVCSIntegrationTest\.git";
 
     public GitRepository(string repositoryPath)
     {
-        gitRepo = new Repository("C:\\Users\\terryn\\Documents\\Visual Studio 2015\\Projects\\UnityVCSIntegrationTest\\.git");
-    }
-
-    private List<string> GetIndex()
-    {
-        List<string> results = new List<string>();
-
-        var untracked = gitRepo.Index.RetrieveStatus().Untracked;
-        return results;
+        
     }
 
     public IEnumerable<WorkingFile> GetStatus()
     {
-        IEnumerable<StatusEntry> repoStatus = gitRepo.Index.RetrieveStatus();
-
         List<WorkingFile> entries = new List<WorkingFile>();
 
-        var flags = FileStatus.Modified | FileStatus.Removed | FileStatus.Added | FileStatus.Untracked;
-
-        var notIg = from ent in repoStatus
-                    where (ent.State & flags) != 0
-                    select ent;
-
-        foreach (var entry in notIg)
+        using (var gitRepo = new Repository(gitRepoPath))
         {
-            WorkingFile tempFile = new WorkingFile();
+            IEnumerable<StatusEntry> repoStatus = gitRepo.Index.RetrieveStatus();
+            var flags = FileStatus.Modified | FileStatus.Removed | FileStatus.Added | FileStatus.Untracked;
+            var notIg = from ent in repoStatus
+                        where (ent.State & flags) != 0
+                        select ent;
 
-            tempFile.filePath = entry.FilePath;
-            switch(entry.State)
+            foreach (var entry in notIg)
             {
-                case FileStatus.Untracked:
-                    tempFile.status = WorkingFile.WorkingStatus.NOTRACK;
-                    break;
-                default:
-                    tempFile.status = WorkingFile.WorkingStatus.PENDING;
-                    break;
-            }
+                WorkingFile tempFile = new WorkingFile();
 
-            entries.Add(tempFile);
+                tempFile.filePath = entry.FilePath;
+                switch (entry.State)
+                {
+                    case FileStatus.Untracked:
+                        tempFile.status = WorkingFile.WorkingStatus.NOTRACK;
+                        break;
+                    default:
+                        continue;
+                        break;
+                }
+
+                entries.Add(tempFile);
+            }
         }
 
         return entries;
@@ -75,26 +68,33 @@ public class GitRepository
 
 public class GitWindow : EditorWindow
 {
-    [MenuItem("Window/Guity")]
-    public static void ShowWindow()
-    {
-        EditorWindow.GetWindow(typeof(GitWindow));
-    }
-
     GitRepository repo;
+
+    bool showStatus;
+
+    float refreshInterval;      // time interval between status refreshes (seconds)
+    float refreshTimer;         // timer counting down to next refresh (seconds)
 
     void OnEnable()
     {
         repo = new GitRepository(Application.dataPath);
+
+        refreshTimer = refreshInterval;
     }
 
     void OnGUI()
     {
+        showStatus = EditorGUILayout.Foldout(showStatus, "Status");
+        if(showStatus)
+        {
+            //EditorGUILayout.LabelField();
+        }
+
+        // WD/Index Toolbar
         EditorGUILayout.BeginHorizontal();
 
         if(GUILayout.Button("status"))
         {
-
             foreach(var entry in repo.GetStatus())
             {
                 Debug.Log(entry.status + " - " + entry.filePath);
@@ -102,5 +102,11 @@ public class GitWindow : EditorWindow
         }
 
         EditorGUILayout.EndHorizontal();
+    }
+
+    [MenuItem("Window/Guity")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(GitWindow));
     }
 }
